7337da52c53ae4f8890a37cf9a24e73bfe039fc7
Zulu <zulu.devs@gmail.com>
2024-06-06 17:51:45 +0100

    create the auth folder for bot sign and sign-up, also design the onboarding screen

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,22 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { image } from "../../constants";
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View className="w-full flex justify-center min-h-[86vh] px-4 my-6">
+          <Image
+            source={image.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,45 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { image } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { router } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 border-2 border-red-300 justify-center items-center">
-      <Text className="text-3xl font-llight text-primary-100">FlexFury</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="flex items-center w-full px-4 min-h-[84vh]">
+          <Image
+            source={image.onboard}
+            resizeMethod="contain"
+            className="h-130px"
+          />
+
+          <View className="mt-5 w-[69%]">
+            <Text className="font-lbold text-[24px] leading-7 capitalize text-center">
+              Wherever you are <Text className="text-primary-100">health</Text>{" "}
+              is number one
+            </Text>
+            <Text className="mt-4 text-[15px] font-llight leading-[18px]">
+              There is no instant way to a healthy life
+            </Text>
+          </View>
+
+          <Image
+            source={image.carousel}
+            resizeMode="contain"
+            className="w-[65px] h-[5px] mt-5"
+          />
+
+          <CustomButton
+            title={"Get Started"}
+            handlePress={() => router.push("/sign-up")}
+            containerStyles={"w-full rounded-[32px] mt-5"}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
